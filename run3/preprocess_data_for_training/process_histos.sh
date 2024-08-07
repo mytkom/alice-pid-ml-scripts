c3e56e3f9a27d00e6b0b44b5e8e70456d225606c
mytkom <marek.mytkowski.mm@gmail.com>
2024-08-07 15:46:20 +0200

    remve falsy comments from bash scripts

--- run3/preprocess_data_for_training/process_histos.sh
+++ run3/preprocess_data_for_training/process_histos.sh
@@ -3,8 +3,6 @@
 script="processHistograms.cpp"
 dir="$3"
 
-# Create the output directory if it doesn't exist
 mkdir -p "$dir"
 
-# Pass the list of .root files to the ROOT script
 root -l -b -q "${script}(\"$1\", \"$2\", \"${dir}\")"
--- run3/preprocess_data_for_training/save_histos.sh
+++ run3/preprocess_data_for_training/save_histos.sh
@@ -4,9 +4,7 @@ script="save_histos.cpp"
 subDir=$1
 dir="$subDir"
 
-# Create the output directory if it doesn't exist
 mkdir -p "$dir"
 
-# Pass the list of .root files to the ROOT script
 root -l -b -q "${script}(\"${subDir}\", \"${dir}\")"
 
